a886bc9759a0cb8dfbe0c5fc1e7b33318eae14b3
ming <amoebami@users.noreply.github.com>
2014-11-04 07:41:11 -0800

    finial version with some minimal testing

--- ImmutableQueue/src/jp/co/worksap/global/ImmutableQueue.java
+++ ImmutableQueue/src/jp/co/worksap/global/ImmutableQueue.java
@@ -5,18 +5,178 @@
  */
 package jp.co.worksap.global;
 
+import java.util.NoSuchElementException;
+
 /**
  *
  * @author ming
  */
-public class ImmutableQueue {
+/**
+ * The Queue class represents an immutable first-in-first-out (FIFO) queue of
+ * objects.
+ * <p>
+ * @param <E>
+ */
+public class ImmutableQueue<E> {
+
+    private static class ImmutableList<E> {
+
+	private final E front;
+	private final ImmutableList<E> end;
+	private int size;
+
+	private ImmutableList()
+	{
+	    this.front = null;
+	    this.end = null;
+	    this.size = 0;
+	}
+
+	private ImmutableList(E front, ImmutableList<E> end)
+	{
+	    this.front = front;
+	    this.end = end;
+	    this.size = end.size + 1;
+	}
+
+	public static ImmutableList emptyList()
+	{
+	    return new ImmutableList();
+	}
+
+	public ImmutableList<E> add(E elem)
+	{
+	    return new ImmutableList<>(elem, this);
+	}
+
+	public boolean isEmpty()
+	{
+	    return this.size == 0;
+	}
+
+	private ImmutableList<E> reverse()
+	{
+	    ImmutableList<E> revList = new ImmutableList<>();
+	    ImmutableList<E> tail = this;
+	    while (!tail.isEmpty())
+	    {
+		revList = revList.add(tail.front);
+		tail = tail.end;
+	    }
+	    return revList;
+	}
+    }
+
+    private ImmutableList<E> head;
+    private ImmutableList<E> tail;
 
     /**
-     * @param args the command line arguments
+     * requires default constructor.
      */
-    public static void main(String[] args)
+    public ImmutableQueue()
     {
-	// TODO code application logic here
+	// modify this constructor if necessary, but do not remove default constructor
+	this.head = ImmutableList.emptyList();
+	this.tail = ImmutableList.emptyList();
     }
-    
+
+    public ImmutableQueue(ImmutableList<E> head, ImmutableList<E> tail)
+    {
+	this.head = head;
+	this.tail = tail;
+    }
+
+    // add other constructors if necessary
+    /**
+     * Returns the queue that adds an item into the tail of this queue without
+     * modifying this queue.
+     * <pre>
+     * e.g.
+     * When this queue represents the queue (2, 1, 2, 2, 6) and we enqueue the value 4 into this queue,
+     * this method returns a new queue (2, 1, 2, 2, 6, 4)
+     * and this object still represents the queue (2, 1, 2, 2, 6) .
+     * </pre> If the element e is null, throws IllegalArgumentException.
+     * <p>
+     * @param e <p>
+     * @return <p>
+     * @throws IllegalArgumentException
+     */
+    public ImmutableQueue<E> enqueue(E e)
+    {
+	if (e == null)
+	{
+	    throw new IllegalArgumentException();
+	}
+
+	return new ImmutableQueue<>(this.head.add(e), this.tail);
+    }
+
+    /**
+     * Returns the queue that removes the object at the head of this queue
+     * without modifying this queue.
+     * <pre>
+     * e.g.
+     * When this queue represents the queue (7, 1, 3, 3, 5, 1) ,
+     * this method returns a new queue (1, 3, 3, 5, 1)
+     * and this object still represents the queue (7, 1, 3, 3, 5, 1) .
+     * </pre> If this queue is empty, throws java.util.NoSuchElementException.
+     * <p>
+     * @return <p>
+     * @throws java.util.NoSuchElementException
+     */
+    public ImmutableQueue<E> dequeue()
+    {
+	if (this.size() == 0)
+	{
+	    throw new NoSuchElementException();
+	}
+
+	if (!this.tail.isEmpty())
+	{
+	    return new ImmutableQueue<>(this.head, this.tail.end);
+	}
+	else
+	{
+	    return new ImmutableQueue<>(ImmutableList.emptyList(), this.head.reverse().end);
+	}
+
+    }
+
+    /**
+     * Looks at the object which is the head of this queue without removing it
+     * from the queue.
+     * <pre>
+     * e.g.
+     * When this queue represents the queue (7, 1, 3, 3, 5, 1),
+     * this method returns 7 and this object still represents the queue (7, 1, 3, 3, 5, 1)
+     * </pre> If the queue is empty, throws java.util.NoSuchElementException.
+     * <p>
+     * @return <p>
+     * @throws java.util.NoSuchElementException
+     */
+    public E peek()
+    {
+	if (this.size() == 0)
+	{
+	    throw new NoSuchElementException();
+	}
+
+	if (this.tail.isEmpty())
+	{
+	    this.tail = this.head.reverse();
+	    this.head = ImmutableList.emptyList();
+	}
+
+	return this.tail.front;
+    }
+
+    /**
+     * Returns the number of objects in this queue.
+     * <p>
+     * @return
+     */
+    public int size()
+    {
+	return this.head.size + this.tail.size;
+    }
 }
--- ImmutableQueue/src/jp/co/worksap/global/ImmutableQueueTest.java
+++ ImmutableQueue/src/jp/co/worksap/global/ImmutableQueueTest.java
@@ -10,5 +10,59 @@ package jp.co.worksap.global;
  * @author ming
  */
 public class ImmutableQueueTest {
-    
+
+    public static void main(String[] args)
+    {
+
+	ImmutableQueue<Integer> dataInteger = new ImmutableQueue<>();
+	ImmutableQueue<Integer> dataInteger2 = new ImmutableQueue<>();
+	ImmutableQueue<Integer> dataInteger3 = new ImmutableQueue<>();
+//	ImmutableQueue<Double> dataDouble = new ImmutableQueue<>();
+	int num = 0;
+	int max = 10;
+	long current = System.currentTimeMillis();
+
+	while (++num <= max)
+	{
+	    int value = (int) (Math.random() * 100);
+	    System.out.print(value + " ");
+	    dataInteger = dataInteger.enqueue(value);
+	}
+	
+	System.out.println("");
+	System.out.println("Time for enqueue " + max + " randome integer "
+			   + (System.currentTimeMillis() - current) + " miliseconds");
+
+	
+//	while (--num > 0)
+//	{
+//	    System.out.print(dataInteger.peek() + " ");
+//	    dataInteger = dataInteger.dequeue();
+//	}
+//
+//	System.out.println("Time for dequeue " + max + " randome integer "
+//			   + (System.currentTimeMillis() - current) + " miliseconds");
+	
+//	System.out.println("");
+//	System.out.println("size: " + dataInteger.size());
+//	while (dataInteger.size() > 0) {
+//	    System.out.print(dataInteger.peek() +" ");
+//	    dataInteger = dataInteger.dequeue();
+//	}
+	
+	System.out.println("");
+	dataInteger2 = dataInteger.enqueue(54);
+	while (dataInteger2.size() > 0) {
+	    System.out.print(dataInteger2.peek() +" ");
+	    dataInteger2 = dataInteger2.dequeue();
+	}
+
+	System.out.println("");
+	dataInteger3 = dataInteger.dequeue();
+	while (dataInteger3.size() > 0) {
+	    System.out.print(dataInteger3.peek() +" ");
+	    dataInteger3 = dataInteger3.dequeue();
+	}
+    }
+
 }
